94fc966e97025c575eb50f23f41a3983312a5e28
ila-park <ila@swit.io>
2023-12-05 12:26:10 +0900

    e

---
+++ packages/preset-ui/src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './button';
+export * from './input';
+export * from './label';
+export * from './list';
+export * from './overlay';
+export * from './paragraph';
+export * from './separator';
