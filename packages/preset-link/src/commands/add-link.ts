3a7b26c401c49dcf0157b8fcebdffd92c3f248a8
ila-park <ila@swit.io>
2023-12-05 19:05:30 +0900

    e

--- packages/preset-link/src/commands/add-link.ts
+++ packages/preset-link/src/commands/add-link.ts
@@ -1,15 +1,5 @@
 import { EditorState, Transaction } from 'prosemirror-state';
 
-export const canAddLink = (state: EditorState): boolean => {
-  const { $from, $to } = state.selection;
-
-  if ($from.parent !== $to.parent) {
-    return false;
-  }
-
-  return $from.parent.isTextblock;
-};
-
 export const addLink = (
   tr: Transaction,
   from: number,
