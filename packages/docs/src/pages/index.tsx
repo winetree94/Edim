241b53f19dee237b80d492c2e040e24919e7e4aa
winetree94 <winetree94@gmail.com>
2023-12-07 03:00:07 +0900

    e

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,38 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import {
-  maximumPlugins,
-  maximumSchema,
-} from '@site/src/components/editor/schemas/maximum';
-import { EditorState } from 'prosemirror-state';
-import { ProseMirror } from '@site/src/components/editor/prose-mirror';
 import styles from './index.module.scss';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
 
-  const [editorState, setEditorState] = useState<EditorState>(
-    EditorState.create({
-      schema: maximumSchema,
-      plugins: maximumPlugins,
-    }),
-  );
-
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-      wrapperClassName={styles.EditorContainer}
-    >
-      <div className={styles.EditorWrapper}>
-        <ProseMirror
-          state={editorState}
-          onStateChange={(state) => setEditorState(state)}
-          attributes={{ spellCheck: 'false' }}
-          className={styles.Editor}
-        ></ProseMirror>
-      </div>
-    </Layout>
+    <BrowserOnly fallback={<div>loading</div>}>
+      {() => {
+        const Edim = require('@site/src/components/editor/edim').Edim;
+        return (
+          <Layout
+            title={`Hello from ${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />"
+            wrapperClassName={styles.EditorContainer}
+          >
+            <div className={styles.EditorWrapper}>
+              <Edim></Edim>
+            </div>
+          </Layout>
+        );
+      }}
+    </BrowserOnly>
   );
 }
--- packages/preset-menubar/src/utils/current-align-type.ts
+++ packages/preset-menubar/src/utils/current-align-type.ts
@@ -5,15 +5,15 @@ export const getAlignType = (state: EditorState) => {
   const { selection } = state;
   const { $from, $to } = selection;
 
-  const children = getBlockContainerChildren(state.doc, $from.pos, $to.pos);
+  // const children = getBlockContainerChildren(state.doc, $from.pos, $to.pos);
 
-  if (
-    $from.parent === $to.parent &&
-    $from.parent.type === state.schema.nodes['heading']
-  ) {
-    const attrs = $from.parent.attrs;
-    return `h${attrs.level}`;
-  }
+  // if (
+  //   $from.parent === $to.parent &&
+  //   $from.parent.type === state.schema.nodes['heading']
+  // ) {
+  //   const attrs = $from.parent.attrs;
+  //   return `h${attrs.level}`;
+  // }
 
   return 'p';
 };
