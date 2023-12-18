2a800e994ed8fc03028907e6da4a3db55428009c
winetree94 <winetree94@gmail.com>
2023-12-18 20:19:11 +0900

    d

--- packages/core/src/plugins/keymap.ts
+++ packages/core/src/plugins/keymap.ts
@@ -1,29 +1,11 @@
 import { keymap } from 'prosemirror-keymap';
-import { undoInputRule } from 'prosemirror-inputrules';
-import {
-  baseKeymap,
-  joinDown,
-  joinUp,
-  lift,
-  selectParentNode,
-} from 'prosemirror-commands';
+import { baseKeymap } from 'prosemirror-commands';
 import { Plugin } from 'prosemirror-state';
-import { clearMarks } from '../commands';
 
 export interface EdimBasicKeymapPluginConfigs {}
 
 export const edimBasicKeymapPlugins = (
   configs?: EdimBasicKeymapPluginConfigs,
 ): Plugin[] => {
-  return [
-    keymap({
-      Backspace: undoInputRule,
-      'Alt-ArrowUp': joinUp,
-      'Alt-ArrowDown': joinDown,
-      'Mod-BracketLeft': lift,
-      Escape: selectParentNode,
-      'Mod-\\': clearMarks(),
-    }),
-    keymap(baseKeymap),
-  ];
+  return [keymap(baseKeymap)];
 };
