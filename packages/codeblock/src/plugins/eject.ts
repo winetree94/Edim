743491b2e796d0c1e71a10ebffd5a2efdd758f67
winetree94 <winetree94@gmail.com>
2023-12-23 19:39:27 +0900

    e

--- packages/codeblock/src/plugins/eject.ts
+++ packages/codeblock/src/plugins/eject.ts
@@ -1,6 +1,7 @@
 import { Plugin as PMPlugin } from 'prosemirror-state';
 import { NodeType } from 'prosemirror-model';
 import { isInCodeBlock } from '../utils';
+import { findParentNode } from 'prosemirror-utils';
 
 export interface EdimCodeBlockEjectPluginConfigs {
   nodeType: NodeType;
@@ -16,10 +17,39 @@ export const edimCodeBlockEjectPlugins = (
           if (!['ArrowRight', 'ArrowDown'].includes(event.key)) {
             return;
           }
-          const codeblock = isInCodeBlock(view.state, configs.nodeType);
-          if (!isInCodeBlock) {
-            return;
+          if (!isInCodeBlock(view.state, configs.nodeType)) {
+            return false;
           }
+
+          const { from, to } = view.state.selection;
+          if (from !== to) {
+            return false;
+          }
+
+          const codeblock = findParentNode(
+            (node) => node.type === configs.nodeType,
+          )(view.state.selection);
+
+          if (!codeblock) {
+            return false;
+          }
+
+          const isLastPos =
+            codeblock.pos + codeblock.node.nodeSize - 1 === from;
+
+          if (!isLastPos) {
+            return false;
+          }
+
+          let tr = view.state.tr;
+
+          tr = tr.insert(
+            codeblock.pos + codeblock.node.nodeSize,
+            view.state.schema.nodes['paragraph'].create(),
+          );
+
+          view.dispatch(tr.scrollIntoView());
+
           return false;
         },
       },
