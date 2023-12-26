ae5b2f069e1a4839ba5c39b711c0da8829aae0d7
winetree94 <winetree94@gmail.com>
2023-12-27 02:25:12 +0900

    e

--- packages/ui/src/cdk/overlay/overlay.ts
+++ packages/ui/src/cdk/overlay/overlay.ts
@@ -1,20 +1,4 @@
-import { html } from '../render';
-import { createContext, createRef, render } from 'preact';
-import { createPortal, forwardRef } from 'preact/compat';
-
-export interface OverlayContext {
-  elements: JSX.Element[];
-
-  open(): void;
-
-  close(): void;
-}
-
-const OverlayContext = createContext<OverlayContext>({
-  elements: [],
-  open: () => {},
-  close: () => {},
-});
+import { createPortal } from 'preact/compat';
 
 const overlayContainer = ((): HTMLDivElement => {
   const exist = document.querySelector(
@@ -28,35 +12,6 @@ const overlayContainer = ((): HTMLDivElement => {
   document.body.appendChild(div);
   return div;
 })();
-
-const overlayRendered: JSX.Element[] = [];
-const overlayRef = createRef();
-
-const EdimOverlay2 = forwardRef<void>(() => {
-  const open = () => {};
-  const close = () => {};
-  return html`
-    <${OverlayContext.Provider} value="${{
-      elements: [],
-      open,
-      close,
-    }}">
-    </${OverlayContext.Provider}>
-  `;
-});
-
-export const reRenderOverlay = () => {
-  render(
-    html`
-      <${EdimOverlay2}>
-        ${overlayRendered}
-      </${EdimOverlay2}>
-    `,
-    overlayContainer,
-  );
-};
-
-reRenderOverlay();
 
 export const EdimOverlay = ({ children }: { children: JSX.Element }) => {
   return createPortal(children, overlayContainer);
